aa79cdc99657637cd609be9c30c8be4a67294d4c
Arthur Alexandre Nascimento <estagiario_arthuran@ciasc.sc.gov.br>
2025-06-16 13:44:47 -0300

    Adicinei validaçao de dados :-D

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,90 +1,103 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.example.Main.fibonacciRecursiva;
 
 public class Main {
     private List<Integer> primosRec = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println(fibonacci (5));
-        System.out.println(chamaFibonacciRecursiva (5));
-        System.out.println(primos(12));
-        System.out.println(chamaPrimosRecursivai(12));
-
-
-
-    }public static List<Integer> chamaPrimosRecursivai(int arg){
-        Main m = new Main();
-        return m.primosRecursivai(arg+1,false);
+        System.out.println(fibonacci(5));
+        System.out.println(chamaFibonacciRecursiva(5));
+        System.out.println(primos(20));
+        System.out.println(chamaPrimosRecursivai(20));
     }
-    public List<Integer> primosRecursivai(int argi,boolean ehPrimo){
-        if(ehPrimo) {
+
+    public static List<Integer> chamaPrimosRecursivai(int arg) {
+        if (arg < 2) {
+            throw new IllegalArgumentException("arg precisa ser >= 2 ");
+        }
+        Main m = new Main();
+        return m.primosRecursivai(arg + 1, false);
+    }
+
+    public List<Integer> primosRecursivai(int argi, boolean ehPrimo) {
+        if (ehPrimo) {
             this.primosRec.add(0, argi);
         }
-        argi-=1;
-        if(argi>=2) {
-            primosRecursivaj(argi,2);
+        argi -= 1;
+        if (argi >= 2) {
+            primosRecursivaj(argi, 2);
         }
         return this.primosRec;
     }
-    public void primosRecursivaj(int argi,int argj){
+
+    public void primosRecursivaj(int argi, int argj) {
         if (argi == 2) {
             primosRecursivai(2, true);
             return;
         }
-        if(argi%argj == 0 ){
-            primosRecursivai(argi,false);
+        if (argi % argj == 0) {
+            primosRecursivai(argi, false);
             return;
         }
-        if(argj <= Math.sqrt(argi)){
-            primosRecursivaj(argi,argj +1);
+        if (argj <= Math.sqrt(argi)) {
+            primosRecursivaj(argi, argj + 1);
             return;
         }
-        primosRecursivai(argi,true);
+        primosRecursivai(argi, true);
     }
 
-    public static List<Integer> primos(int arg){
-        boolean ehPrimo = false;
+    public static List<Integer> primos(int arg) {
+        if (arg < 2) {
+            throw new IllegalArgumentException("arg precisa ser >= 2 ");
+        }
+        boolean ehPrimo;
         List<Integer> primos = new ArrayList<>();
-        for(int i =2; i <= arg ; i++) {
-            ehPrimo =true;
+        for (int i = 2; i <= arg; i++) {
+            ehPrimo = true;
             for (int j = 2; j <= Math.sqrt(i); j++) {
-                if(i%j == 0 ){
+                if (i % j == 0) {
                     ehPrimo = false;
                     break;
                 }
             }
-            if(ehPrimo){
+            if (ehPrimo) {
                 primos.add(i);
             }
         }
         return primos;
     }
 
-    public static int fibonacciRecursiva(int atual, int anterior,int arg){
-        if(arg>0){
+    public static int fibonacciRecursiva(int atual, int anterior, int arg) {
+        if (arg > 0) {
             atual += anterior;
-            anterior = atual-anterior;
-            return fibonacciRecursiva(atual,anterior,arg - 1);
+            anterior = atual - anterior;
+            return fibonacciRecursiva(atual, anterior, arg - 1);
         }
         return anterior;
     }
 
-    public static int chamaFibonacciRecursiva(int arg){
-        int atual=1;
-        int anterior=0;
-        return fibonacciRecursiva(atual,anterior,arg);
+    public static int chamaFibonacciRecursiva(int arg) {
+        if (arg < 0) {
+            throw new IllegalArgumentException("arg precisa ser >= 0 ");
+        }
+        int atual = 1;
+        int anterior = 0;
+        return fibonacciRecursiva(atual, anterior, arg);
     }
 
-    public static int fibonacci (int arg){
-        int atual =1;
-        int anterior =0;
+    public static int fibonacci(int arg) {
+        if (arg < 0) {
+            throw new IllegalArgumentException("arg precisa ser > 0 ");
+        }
+        int atual = 1;
+        int anterior = 0;
         for (int i = arg; i > 0; i--) {
             atual += anterior;
-            anterior = atual-anterior;
+            anterior = atual - anterior;
         }
         return anterior;
     }
